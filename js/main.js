89a69e5b35581f33ce2038640c345d4ab6313e3c
petbaik <petbaik26@gmail.com>
2017-05-21 13:22:46 +0300

    Home page

--- js/main.js
+++ js/main.js
@@ -18,11 +18,20 @@ $(document).ready(function(){
     $(window).scroll(function(){
       if(typeof page != "undefined" && page == "home"){
           if($(window).scrollTop() >= $('#headerBG').height()){
-            $('nav').css({position:'fixed',width:'100%',background:'#234465',zIndex:'10'});
+          /*  $('#home-header').css({marginTop:$('nav').height()});*/
+            $('nav').fadeIn().css({position:'fixed',width:'100%',background:'#234465',zIndex:'10'});
+            $('#left-nav').css({position:'fixed',top:'20%'});
           }else{
             $('nav').removeAttr('style');
+            $('#left-nav').removeAttr('style');
           }
       }
 
     });
 });
+function scrollToSection(self){
+  var id = $(self).attr('id');
+  $('html, body').animate({
+    scrollTop:$('.'+id).offset().top
+  },2000);
+}
